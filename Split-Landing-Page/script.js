fed58c09227d0a8566a894be1e409073d949ef17
Stephanie-17 <Stephanie>
2025-06-09 17:02:02 +0100

    first commit

---
+++ Split-Landing-Page/script.js
@@ -0,0 +1,15 @@
+document.querySelector('.landing-page1').addEventListener('mouseenter',()=>{
+    document.querySelector('.landing-page1').classList.add('active-page')
+})
+document.querySelector('.landing-page1').addEventListener('mouseleave',()=>{
+    document.querySelector('.landing-page1').classList.remove('active-page')
+})
+
+document.querySelector('.landing-page2').addEventListener('mouseenter',()=>{
+    document.querySelector('.landing-page2').classList.add('active-page')
+})
+
+document.querySelector('.landing-page2').addEventListener('mouseleave',()=>{
+    document.querySelector('.landing-page2').classList.remove('active-page')
+})
+
